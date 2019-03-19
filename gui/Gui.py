47a2feb93b73ce5a0ee9528c6399fbe8c321b3c3
Henrik Talarmo <talarmo.henrik@gmail.com>
2019-03-19 17:43:13 +0200

    Added buttons to the gui

--- gui/Gui.py
+++ gui/Gui.py
@@ -1,6 +1,9 @@
+import os
+
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
-from PyQt5.QtWidgets import QLineEdit, QLabel, QSpacerItem
+from PyQt5.QtWidgets import QLineEdit, QLabel, QSpacerItem, QPushButton
 from PyQt5.QtWidgets import QGridLayout, QVBoxLayout, QHBoxLayout
+from PyQt5.QtWidgets import QFileDialog
 from PyQt5.QtCore import Qt
 
 """
@@ -26,18 +29,38 @@ class Gui:
 		self.main_layout = QVBoxLayout()
 		self.main_widget.setLayout(self.main_layout)
 
-		# Creating the input widget
+		# Creating the input widget ########################33
 		self.input_widget = QWidget()
 		self.program_line_edit = QLineEdit()
 		self.test_input_line_edit = QLineEdit()
 		self.program_label = QLabel("Program path")
 		self.test_input_label = QLabel("Input path")
+		
+		# browse program path button
+		self.program_path_widget = QWidget()
+		self.program_path_button = QPushButton("Browse")
+		self.program_path_spacer = QSpacerItem(300, 0)
+		self.program_path_layout = QHBoxLayout()
+		self.program_path_layout.addWidget(self.program_path_button)
+		self.program_path_layout.addSpacerItem(self.program_path_spacer)
+		self.program_path_widget.setLayout(self.program_path_layout)
+
+		# browse input file path button
+		self.input_path_widget = QWidget()
+		self.input_path_button = QPushButton("Browse")
+		self.input_path_spacer = QSpacerItem(300, 0)
+		self.input_path_layout = QHBoxLayout()
+		self.input_path_layout.addWidget(self.input_path_button)
+		self.input_path_layout.addSpacerItem(self.input_path_spacer)
+		self.input_path_widget.setLayout(self.input_path_layout)
 
 		self.input_layout = QGridLayout()
 		self.input_layout.addWidget(self.program_label, 0, 0)
 		self.input_layout.addWidget(self.program_line_edit, 1, 0)
+		self.input_layout.addWidget(self.program_path_widget, 2, 0)
 		self.input_layout.addWidget(self.test_input_label, 0, 1)
 		self.input_layout.addWidget(self.test_input_line_edit, 1, 1)
+		self.input_layout.addWidget(self.input_path_widget, 2, 1)
 		self.input_widget.setLayout(self.input_layout)
 
 		# Main widget spacer
@@ -45,12 +68,17 @@ class Gui:
 
 		# Set widgets into main widget
 		self.main_layout.addWidget(self.input_widget)
-		#self.main_layout.addWidget(self.main_spacer)
+		self.main_layout.addSpacerItem(self.main_spacer)
 
 		self.main_window.setCentralWidget(self.main_widget)
 
 		self.main_window.show()
 		self.app.exec_()
+
+	def create_file_browser(self):
+		self.cwd = os.getcwd()
+		self.file_dialog = QFileDialog()
+
 
 
 if __name__ == "__main__":
