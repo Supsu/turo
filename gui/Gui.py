aaccf43f1e68e8689191ecd1b652980297096350
Henrik Talarmo <talarmo.henrik@gmail.com>
2019-03-21 21:40:44 +0200

    Added more Gui and some blank functions

--- gui/Gui.py
+++ gui/Gui.py
@@ -1,7 +1,7 @@
 import os
 
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
-from PyQt5.QtWidgets import QLineEdit, QLabel, QSpacerItem, QPushButton
+from PyQt5.QtWidgets import QLineEdit, QLabel, QSpacerItem, QPushButton, QTextEdit, QProgressBar
 from PyQt5.QtWidgets import QGridLayout, QVBoxLayout, QHBoxLayout
 from PyQt5.QtWidgets import QFileDialog
 from PyQt5.QtCore import Qt
@@ -14,6 +14,7 @@ class Gui:
 	def __init__(self):
 		self.app = None
 		self.main_window = None
+		
 
 	"""
 	Creates a new main window and opens it
@@ -29,9 +30,39 @@ class Gui:
 		self.main_layout = QVBoxLayout()
 		self.main_widget.setLayout(self.main_layout)
 
-		# Creating the input widget ########################
-		# variables related to program input gui features start with "pr"
-		# and variables related to test input begin with "ti"
+		"""
+			Splitting creating different bits of the gui into separate
+			functions for the sake of keeping the create_window method
+			at least somewhat manageable in size.
+		"""
+		self.create_input_widget()
+		self.create_status_widget()
+
+		# Main widget spacer
+		self.main_spacer = QSpacerItem(0, 0)
+
+		# Set widgets into main widget
+		self.main_layout.addWidget(self.input_widget)
+		self.main_layout.addSpacerItem(self.main_spacer)
+		self.main_layout.addWidget(self.status_widget)
+
+		self.main_window.setCentralWidget(self.main_widget)
+
+		# Connect all the buttons to their functions
+		self.pr_browse_button.clicked.connect(self.browse_for_program)
+		self.ti_browse_button.clicked.connect(self.browse_for_input_file)
+		self.status_run.clicked.connect(self.run)
+		self.status_stop.clicked.connect(self.stop)
+		self.status_config.clicked.connect(self.config)
+		self.status_exit.clicked.connect(self.exit)
+
+		self.main_window.show()
+		self.app.exec_()
+
+	"""
+	Creates the input widget for the main window
+	"""
+	def create_input_widget(self):
 		self.input_widget = QWidget()
 		self.pr_path = QLineEdit()
 		self.ti_path = QLineEdit()
@@ -65,21 +96,40 @@ class Gui:
 		self.input_layout.addWidget(self.ti_browse_widget, 2, 1)
 		self.input_widget.setLayout(self.input_layout)
 
-		# Main widget spacer
-		self.main_spacer = QSpacerItem(800, 600)
+	def create_status_widget(self):
+		self.status_widget = QWidget()
+		self.status_buttons = QWidget()
+		self.status_log_widget = QWidget()
+		self.status_log = QTextEdit()
+		self.status_progress = QProgressBar()
+		self.status_run = QPushButton("Run")
+		self.status_stop = QPushButton("Stop")
+		self.status_config = QPushButton("Config")
+		self.status_exit = QPushButton("Exit")
 
-		# Set widgets into main widget
-		self.main_layout.addWidget(self.input_widget)
-		self.main_layout.addSpacerItem(self.main_spacer)
+		self.status_log.setReadOnly(True)
+		self.status_progress.setMinimum(0)
+		self.status_progress.setMaximum(10)
 
-		self.main_window.setCentralWidget(self.main_widget)
+		self.status_layout = QHBoxLayout()
+		self.status_button_layout = QVBoxLayout()
+		self.status_log_layout = QVBoxLayout()
 
-		# Connect all the buttons to their functions
-		self.pr_browse_button.clicked.connect(self.browse_for_program)
-		self.ti_browse_button.clicked.connect(self.browse_for_input_file)
+		self.status_button_layout.addWidget(self.status_run)
+		self.status_button_layout.addWidget(self.status_stop)
+		self.status_button_layout.addWidget(self.status_config)
+		self.status_button_layout.addSpacerItem(QSpacerItem(0,300))
+		self.status_button_layout.addWidget(self.status_exit)
+		self.status_buttons.setLayout(self.status_button_layout)
 
-		self.main_window.show()
-		self.app.exec_()
+		self.status_log_layout.addWidget(self.status_log)
+		self.status_log_layout.addWidget(self.status_progress)
+		self.status_log_widget.setLayout(self.status_log_layout)
+
+		self.status_layout.addWidget(self.status_buttons)
+		self.status_layout.addWidget(self.status_log_widget)
+		self.status_widget.setLayout(self.status_layout)
+
 
 	"""
 	Creates a file dialog at the current working directory and 
@@ -104,6 +154,17 @@ class Gui:
 		if len(self.chosen_file) != 0:
 			self.ti_path.setText(self.chosen_file)
 
+	def run(self):
+		print("Run")
+
+	def stop(self):
+		print("Stop")
+
+	def config(self):
+		print("Config")
+
+	def exit(self):
+		exit()
 
 if __name__ == "__main__":
 	a = Gui()
