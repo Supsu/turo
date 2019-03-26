c5696e61df382969528dd7fc7eecd6dad926b740
Henrik Talarmo <talarmo.henrik@gmail.com>
2019-03-26 14:59:35 +0200

    Added shlex

--- controller/controller.py
+++ controller/controller.py
@@ -1,18 +1,24 @@
-import asyncio
+#import asyncio TODO: event loop in here or operation handler?
+import shlex
 from ..fuzzer.operationhandler import OperationHandler
 
 class Controller:
 
-	def __init__(self):
+	def __init__(self, args):
 		self.operationhandler = None
+		self.args = args
 
 	# TODO: Sanitation needed for filepaths?
 	#       It comes from Qt's widget so one would think that that sanitizes it...
 	def run(self, input_path, program_path, config_data):
 		self.config = config_data
 
-		self.operationhandler = OperationHandler(self.input_path, program_path)
+		# Run shlex for args
+		# TODO: Check this later
+		self.passed_args = shlex.split(self.args)
+		self.operationhandler = OperationHandler(self.input_path, program_path, self.passed_args
 
+		self.operationhandler.run()
 
 
 
