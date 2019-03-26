077cf178111827e06d79ad316bbbb4f349420955
Henrik Talarmo <talarmo.henrik@gmail.com>
2019-03-26 14:20:10 +0200

    Added initial controller

---
+++ controller/controller.py
@@ -0,0 +1,17 @@
+import asyncio
+
+class Controller:
+
+	def __init__(self):
+		self.input_file = ""
+		self.program_file = ""
+		self.config = None # TODO: import ConfigData from gui
+
+	def set_input_file(self, filename):
+		self.input_file = filename
+		
+
+
+
+if __name__ == "__main__":
+	a = Controller()
