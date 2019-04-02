dd5ec7e0deb757bfff6deda315b5d7309718d3e1
Henrik Talarmo <talarmo.henrik@gmail.com>
2019-04-02 16:49:06 +0300

    Added log event signals and slots to controller

--- controller/controller.py
+++ controller/controller.py
@@ -3,16 +3,25 @@ from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot
 from ..fuzzer.operationhandler import OperationHandler
 # TODO: import ConfigData
 
-class Controller:
+"""
+Controller for the program. Creates OperationHandler and receives the config from GUI.
+
+run() starts the program. 
+Emits a log_Event signal when it wants to send a signal to the GUI to add a log event
+to it's log window.
+"""
+class Controller(QObject):
+	log_event = pyqtSignal(int, str)
 
 	def __init__(self, args):
+		super(Controller, self).__init__()
 		self.operationhandler = None
 		self.args = args
 		self.sendLogMessage = pyqtSignal(str)
 
 	# TODO: Sanitation needed for filepaths?
 	#       It comes from Qt's widget so one would think that that sanitizes it...
-	def run(self, input_path, program_path, config_data):
+	def run(self, input_path, program_path, config_data):	
 
 		# Run shlex for args
 		# TODO: Check this later
@@ -21,6 +30,12 @@ class Controller:
 		self.operationhandler = OperationHandler(input_path, program_path)
 
 		self.operationhandler.run()
+
+	"""
+	Slot for receiving a log signal from OperationHandler
+	"""
+	def log_received(self, ts, event):
+		self.log_event.emit(ts, event)
 
 if __name__ == "__main__":
 	a = Controller("123")
