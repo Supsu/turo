bfc403031e34368554b6a0c8ed21ec79d524ee23
Henrik Talarmo <talarmo.henrik@gmail.com>
2019-04-02 15:52:20 +0300

    Controller is now as ready it can be before gui is merged into master

--- controller/controller.py
+++ controller/controller.py
@@ -1,25 +1,27 @@
-#import asyncio TODO: event loop in here or operation handler?
 import shlex
-from fuzzer.operationhandler import OperationHandler
+from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot
+from ..fuzzer.operationhandler import OperationHandler
+# TODO: import ConfigData
 
 class Controller:
 
 	def __init__(self, args):
 		self.operationhandler = None
 		self.args = args
+		self.sendLogMessage = pyqtSignal(str)
 
 	# TODO: Sanitation needed for filepaths?
 	#       It comes from Qt's widget so one would think that that sanitizes it...
 	def run(self, input_path, program_path, config_data):
-		self.config = config_data
 
 		# Run shlex for args
 		# TODO: Check this later
 		self.passed_args = shlex.split(self.args)
-		self
+		
 		self.operationhandler = OperationHandler(input_path, program_path)
 
 		self.operationhandler.run()
 
 if __name__ == "__main__":
 	a = Controller("123")
+	a.run("", "", None)
