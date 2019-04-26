b30e9ea2ad0bc53eabc0ecfb330a4a78aa701b52
Tommi Nikula <tommi.nikula@optofidelity.com>
2019-04-26 14:24:01 +0300

    Use bytes

--- fuzzer/mutator.py
+++ fuzzer/mutator.py
@@ -1,10 +1,19 @@
 """Mutator takes care of mutating the program inputs. """
 import random
 
-
 class Mutator:
     def __init__(self, initial_input, number_of_mutations=10):
-        self._initial_input = initial_input
+
+        try:
+            self._initial_input = bytearray(initial_input, "utf-8")
+        except TypeError:
+            self._initial_input = initial_input
+
+        
+
+        if type(self._initial_input) is not bytearray:
+            raise TypeError("Mutator expected a bytearray or a string")
+
         self._number_of_mutations = number_of_mutations
         self._mutated = 0
         self._output = self._initial_input
@@ -17,7 +26,7 @@ class Mutator:
         return output
 
 if __name__ == "__main__":
-    mutator = Mutator("abcdefg")
+    mutator = Mutator(66)
 
     for i in range(10):
         print(mutator.mutate())
